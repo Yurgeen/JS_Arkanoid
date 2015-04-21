67bd09d2bedf2caf68f627bd67bff0f999a175b1
Yurgeen <yuriy.raychinets@gmail.com>
2015-04-21 23:45:22 +0300

    [ResourceManager] Proper image and sprite loading

--- engine/resourceManager.js
+++ engine/resourceManager.js
@@ -8,7 +8,7 @@ ResourceManager = {
         "resources/audio.json"
     ],
 
-    init : function() {
+    init : function(callback) {
         var me = this;
 
         me.graphics = {};
@@ -28,13 +28,14 @@ ResourceManager = {
                 config = JSON.parse(req.responseText);
                 for (var img in config) {
                     var image = new Image();
-                    image.onload = function () {
-                        if (config.sprite) {
-                            me.graphics[img] = [];
-                        } else {
-                            me.graphics[img] = [image];
+                    image.onload = (function(img, sprite) {
+                        return function () {
+                            if (sprite) {
+                                me.graphics[img] = me.makeSpiteArray(image, sprite.frameWidth, sprite.frameHeight, sprite.count);
+                            } else {
+                                me.graphics[img] = [image];
                         }
-                    };
+                    }})(img, config[img].sprite);
                     image.src = config[img].image;
                 }
             }
@@ -48,7 +49,39 @@ ResourceManager = {
 
     },
 
-    processSprite : function(image, spriteConfig) {
 
+    makeSpiteArray : function (image, frameWidth, frameHeight, cnt)
+    {
+        var canvas = document.createElement('canvas'),
+            ctx = canvas.getContext("2d"),
+            r = Math.floor(image.width/frameWidth),
+            c = Math.floor(image.height/frameHeight),
+            n = 0, result = [];
+
+        canvas.width = 	image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0);
+
+        for (var i = 0; i < r; i++) {
+            for (var j = 0; j < c; j++) {
+                var partCanvas = document.createElement('canvas'),
+                    partCtx = partCanvas.getContext("2d"),
+                    ImgData = ctx.getImageData(j*frameWidth, i*frameHeight, frameWidth, frameHeight);
+
+                partCanvas.width = 	frameWidth;
+                partCanvas.height = frameHeight;
+                partCtx.putImageData(ImgData, 0, 0);
+                result.push(partCanvas);
+                n++;
+                if (n > cnt) break;
+            }
+        }
+        return result;
+    },
+
+    checkLoad : function() {
+        var me = this;
+
+ //       me.callback();
     }
 }
