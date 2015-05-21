bc70327af7aa507cc78ca651345828f76668abc0
yuriy.raichynets <yuriy.raichynets@gmail.com>
2015-05-21 12:49:13 +0300

    Used one canvas for all bg

--- modules/backgound.js
+++ modules/backgound.js
@@ -1,47 +1,59 @@
 function Background() {
     var me = this,
-        pattern = ResourceManager.graphics["sky"],
-        frameHeight = pattern.image.height,
-        frameWidth = pattern.image.width,
-        sceneWidth = AnimationManager.width,
-        sceneHeight = AnimationManager.height,
+        bgItem, bgCtx;
+
+    me.fps = 120;
+
+    bgItem = new AnimationItem({
+        image: me.createBgPattern(),
+        x: 0,
+        y: 0
+    });
+
+    bgCtx = bgItem.image.getContext("2d");
+
+    bgItem.animationFunction = function() {
+        me.redrawPattern(bgCtx, this.localTime);
+    };
+
+    me.item = bgItem;
+    AnimationManager.addItem(me.item);
+}
+
+Background.prototype.createBgPattern = function(){
+    var me = this,
+        canvas = document.createElement("canvas"),
+        ctx = canvas.getContext("2d");
+
+    canvas.width = AnimationManager.width;
+    canvas.height = AnimationManager.height;
+    me.redrawPattern(ctx, 0);
+    return canvas;
+};
+
+Background.prototype.redrawPattern = function(ctx, time){
+    var me = this,
+        pattern = ResourceManager.graphics["sky"].image,
+        frameHeight = pattern.height,
+        frameWidth = pattern.width,
+        sceneHeight = ctx.canvas.height,
+        sceneWidth = ctx.canvas.width,
         r = Math.floor(sceneHeight/frameHeight),
         c = Math.floor(sceneWidth/frameWidth),
-        tiles = [],
-        fps = 10,
-        lastOffset = sceneHeight - frameHeight* r,
-        item, n;
+        n;
 
+    n = Math.round((time*me.fps/1000) % frameHeight) || 0;
 
     for (var i = -1; i <= r; i++) {
         for(var j = 0; j <= c; j++) {
-            item = new AnimationItem({
-                x: j*frameWidth,
-                y: i*frameHeight,
-                depth: 0,
-                image: pattern.image
-            });
-            item.animationFunction = function() {
-                n = (frameHeight % this.localTime*fps/1000) || 0;
-                this.y += n;
-
-                if (this.y > sceneHeight) {
-                    this.y = lastOffset - sceneHeight + n;
-                }
-            };
-            tiles.push(item);
-            AnimationManager.addItem(item);
+            ctx.drawImage(pattern, j*frameWidth, i*frameHeight + n);
         }
     }
-
-    me.tiles = tiles;
-}
+};
 
 Background.prototype.start = function(){
     var me = this;
-    me.tiles.forEach(function(element){
-       element.play();
-    });
-}
+    me.item.play();
+};
 
 ModuleLoader.registerModule("Background", Background);
